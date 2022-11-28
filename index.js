dba5c2c9a5a290c2fbd8217e8cadebdca6de926f
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-28 15:06:13 -0500

    commit tree 26da9a543d6e1cb1014835e2e37bae1326b2780e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, passedName){
+    return drivers.filter(function(name){
+        return name.toLowerCase() === passedName.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, passedName){
+    return drivers.filter(function(name){
+        return name.startsWith(passedName);
+    })
+}
+
+function matchName(drivers, passedName){
+return drivers.filter(function(driver){
+    return driver.name === passedName;
+})
+}
